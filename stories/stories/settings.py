a435d9e63be645d420d9d1c09b672ad4f825a13d
KavinJey <jeyakavin33@gmail.com>
2018-02-25 10:02:57 -0500

    added CORS

--- stories/stories/settings.py
+++ stories/stories/settings.py
@@ -37,11 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -116,6 +119,7 @@ USE_L10N = True
 USE_TZ = True
 
 ALLOWED_HOSTS = ['stories-api-prod.herokuapp.com' , '127.0.0.1']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
